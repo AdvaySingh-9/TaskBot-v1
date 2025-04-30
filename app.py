590092418eae4a56459c0160317b5b8954ca6e63
Advay Singh <advaysingh2012@gmail.com>
2025-04-30 18:27:57 +0530

    app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def ask():
     try:
         # use Google's Gemini-2.0-Flash nodle for generating content
         model = genai.GenerativeModel('gemini-2.0-flash')
-        response = model.generate_content(f"You are TaskBot AI created and trained by Advay Singh and Astrumix. Remember that and just anser me this question in detail- {question}. And don't reply on that message. Just answer me the question.")
+        response = model.generate_content(f"You are TaskBot AI created and trained by Advay Singh and Astrumix remember that and just anser me this question- {question}. Don't replay anything on that message just answer me the question.")
         answer = response.text
 
         # Log the question and answer for debugging
@@ -35,5 +35,5 @@ def ask():
         print(f"Error: {e}")
         return jsonify({"error": "An error occurred while processing your request."}), 500
 
-if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7860)
